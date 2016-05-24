9af0b1b9f648c637420b002ed7c8449ac95b272c
Ali <toalisaeedmalik@gmail.com>
2016-05-23 21:31:57 -0400

    goes to next scene

---
+++ Assets/testmoveup.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testmoveup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void moveup(){
+		transform.position += new Vector3 (0f, 0.1f, 0f);
+		Application.LoadLevel ("demoScene_free");
+		
+	}
+}
